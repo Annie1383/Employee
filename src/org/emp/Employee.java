1d141977225a7b8a42d5c36b0f974cfd11ed27c5
annie1383 <anni83@gmail.com>
2019-10-16 11:18:13 +0530

    newcode

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,41 @@
+package org.emp;
+public class Employee {
+	
+private void empID()
+{ 
+	System.out.println("20341637");
+	
+}
+private void empName()
+{
+	System.out.println("Annie");
+}
+private void empDob()
+{
+	System.out.println("13-07-1983");
+}
+private void empPhone()
+{
+	System.out.println("9962582300");
+	
+	}	
+private void empEmail()
+{
+	System.out.println("anni83@gmail.com");
+}
+private void empAddress()
+{
+	System.out.println("LM Apartments,Gunidy,Chennai");
+}
+public static void main(String[] args)
+{
+	Employee edt=new Employee();
+	edt.empID();
+	edt.empName();
+	edt.empDob();
+	edt.empPhone();
+	edt.empEmail();
+	edt.empAddress();
+}
+
+}
